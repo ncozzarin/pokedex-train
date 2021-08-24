9e0f5c6702c5de3f031f9d9df45a6ac1979e5a4e
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-08-24 15:09:30 +0200

    Api call fixed

--- src/Layouts/Homescreen.jsx
+++ src/Layouts/Homescreen.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Pokemonlist from '../Components/Pokemonlist';
-import data from "../pokemons.json";
+
 const fetchURL = "https://pokeapi.co/api/v2/";
 export default function HomeScreen() {
-    const [hasError, setErrors] = useState(false);
     const [pokemons, setPokemons] = useState();
-    
-    const getPokemons = () =>
-    fetch(`${fetchURL}/pokemon?limit=10&offset=200`)
-      .then((res) => res.json())
-    
+
     useEffect(() => {
-        getPokemons().then((pokemons) => setPokemons(pokemons))
-      }, [])
+        getPokemons();
+      }, []);
     
+    const getPokemons = async () => {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=200");
+        const data = await response.json();
+        console.log(data); 
+    }
     
     console.log(pokemons);
   
