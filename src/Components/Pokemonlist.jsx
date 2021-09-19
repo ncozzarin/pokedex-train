be5e627be0b37444ee4728b3acbf3c9942d08a91
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-09-19 17:05:31 -0300

    update

--- src/Components/Pokemonlist.jsx
+++ src/Components/Pokemonlist.jsx
@@ -1,27 +1,21 @@
 import React, { useState } from 'react';
-import Preview from './Preview';
+import Preview from './preview.jsx';
 import {getPokemonsData} from '../utils/fetchService';
  
 
-// eslint-disable-next-line react/prop-types
 const Pokemonlist = ({pokemonsJSON}) => {
-    const [pokemons, setPokemons] = useState();
-    
-    setPokemons(pokemonsJSON);
-    console.log(pokemons);
+    const [pokemons, setPokemons] = useState([]);
+
+    setPokemons(JSON.parse(pokemonsJSON));
    return (
-    <div>hola</div>
-/*        <div className="grid grid-cols-3 m-3">
+        <div className="grid grid-cols-3 m-3">
            {pokemons && pokemons.map(pokemon => {
                let data = getPokemonsData(pokemon.url);
                return (
-
-                   <Preview key={pokemonsJSON} pokemon={data}/>
-               )
+                   console.log(data)
+               );
            })}
        </div>
    );
-}; */
-   );
-
+}; 
    export default Pokemonlist;
