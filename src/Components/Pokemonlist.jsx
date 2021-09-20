05c8dbed14b5c216d16510ebe45380aabde943e9
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-09-20 18:29:27 -0300

    Api call fix
    Now the function is defined inside the useEffect

--- src/Components/Pokemonlist.jsx
+++ src/Components/Pokemonlist.jsx
@@ -1,21 +1,18 @@
 import React, { useState } from 'react';
-import Preview from './preview.jsx';
-import {getPokemonsData} from '../utils/fetchService';
- 
+import Preview from '../components-1/preview';
+import { getPokemonsData } from '../utils/fetchService';
 
-const Pokemonlist = ({pokemonsJSON}) => {
-    const [pokemons, setPokemons] = useState([]);
+const Pokemonlist = ({ pokemonsJSON }) => {
 
-    setPokemons(JSON.parse(pokemonsJSON));
-   return (
+    return (
         <div className="grid grid-cols-3 m-3">
-           {pokemons && pokemons.map(pokemon => {
-               let data = getPokemonsData(pokemon.url);
-               return (
-                   console.log(data)
-               );
-           })}
-       </div>
-   );
-}; 
-   export default Pokemonlist;
+        <h2>Pokemon Name</h2>
+            {pokemonsJSON &&
+              pokemonsJSON.map((pokemon) => {
+
+                   return <h1>{pokemon.name}</h1>
+                })}
+        </div>
+    );
+};
+export default Pokemonlist;
--- src/Layouts/Homescreen.jsx
+++ src/Layouts/Homescreen.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Pokemonlist from '../Components/Pokemonlist';
-import {getPokemons} from '../utils/fetchService';
 
 export default function HomeScreen() {
     const [pokemonsJSON, setPokemons] = useState();
 
-    const getData = async () => await getPokemons(10,20);
-    console.log(pokemonsJSON);
-
     /* Use efect will check everytime a diff in the context */
     useEffect(() => {
-      setPokemons(getData());
-      }, []);
-  
-      return (
+        const getPokemons = async (limitNumber, offsetNumber) => {
+            const response = await fetch(
+                `https://pokeapi.co/api/v2/pokemon?limit=${limitNumber}&offset=${offsetNumber}`,
+            );
+            const data = await response.json();
+            setPokemons(data.results);
+        };
 
-         <Pokemonlist pokemonList={pokemonsJSON} ></Pokemonlist>
-    );
-  }
+        getPokemons(20, 10);
+    }, []);
+
+    return pokemonsJSON ? <Pokemonlist pokemonsJSON={pokemonsJSON}></Pokemonlist> : <h1>Error</h1>;
+}
