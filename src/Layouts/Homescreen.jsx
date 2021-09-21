07682215dae6df85a689ff12f1213e6831a6e913
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-09-21 08:26:43 -0300

    Fetch the api in the fetchService file

--- src/Layouts/Homescreen.jsx
+++ src/Layouts/Homescreen.jsx
@@ -1,21 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Pokemonlist from '../Components/Pokemonlist';
+import {getPokemons} from '../utils/fetchService';
 
 export default function HomeScreen() {
     const [pokemonsJSON, setPokemons] = useState();
 
     /* Use efect will check everytime a diff in the context */
     useEffect(() => {
-        const getPokemons = async (limitNumber, offsetNumber) => {
-            const response = await fetch(
-                `https://pokeapi.co/api/v2/pokemon?limit=${limitNumber}&offset=${offsetNumber}`,
-            );
-            const data = await response.json();
-            setPokemons(data.results);
-            
+        const getData = async () => {
+            const response = await getPokemons(20,10);
+            setPokemons(response.results);
         };
 
-        getPokemons(20, 10);
+        getData();
     }, []);
 
     return pokemonsJSON ? <Pokemonlist pokemonsJSON={pokemonsJSON}></Pokemonlist> : <h1>Error</h1>;
