e7e5b64df8e734b2e31658c3d6218e6841534e30
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-09-22 14:14:56 -0300

    Effect dependancy

--- src/Components/Pokemonlist.jsx
+++ src/Components/Pokemonlist.jsx
@@ -6,24 +6,27 @@ import {getPokemonsData, getPokemonColor} from '../utils/fetchService';
 export default function Pokemonlist ({ pokemonsJSON }) {
     const [pokemons, setPokemons] = useState([]);
 
+    const getData = async (url) => {
+        let response = await getPokemonsData(url);
+        response.results.color = await getPokemonColor(response.results.name);
+        setPokemons([...pokemons, response.results]);
+    };
 
     useEffect(() => {
-        const getData = async (url) => {
-            let response = await getPokemonsData(url);
-            response.results.color = await getPokemonColor(response.results.name);
-            setPokemons([...pokemons, response.results]);
-        };
+        getData(pokemon.url);
         
-    },[]);
+    },[pokemons]);
 
     return (
         <div className="grid grid-cols-3 m-3">
         <h2>Pokemon Name</h2>
             {pokemonsJSON &&
               pokemonsJSON.map((pokemon) => {
-                getData(pokemon.url);
                    return (
-                       <h1>{pokemons.name}</h1>
+                       <div>
+                        <h1>{pokemons.name}</h1>
+                        <h2>{pokemons.color}</h2>
+                    </div>
               )})}
         </div>
     );
