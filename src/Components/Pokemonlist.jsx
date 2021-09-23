7df87370b9d94c97475a69c9a43c84a5a0be1afb
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-09-23 08:45:02 -0300

    useEffect updated with logic to fill pokemons state variable

--- src/Components/Pokemonlist.jsx
+++ src/Components/Pokemonlist.jsx
@@ -6,28 +6,33 @@ import {getPokemonsData, getPokemonColor} from '../utils/fetchService';
 export default function Pokemonlist ({ pokemonsJSON }) {
     const [pokemons, setPokemons] = useState([]);
 
-    const getData = async (url) => {
+    const getData = async url => {
         let response = await getPokemonsData(url);
         response.results.color = await getPokemonColor(response.results.name);
         setPokemons([...pokemons, response.results]);
     };
 
     useEffect(() => {
-        getData(pokemon.url);
-        
-    },[pokemons]);
+        console.log(pokemonsJSON);
+        console.log(pokemonsJSON[1].url);
+        pokemonsJSON && pokemonsJSON.map((pokemon) => {
+            getData(pokemon.url);
+        });
+
+    },[pokemonsJSON]);
 
     return (
         <div className="grid grid-cols-3 m-3">
         <h2>Pokemon Name</h2>
-            {pokemonsJSON &&
-              pokemonsJSON.map((pokemon) => {
+        <div>
+        {pokemons && pokemons.map((pokemon) => {
                    return (
                        <div>
-                        <h1>{pokemons.name}</h1>
-                        <h2>{pokemons.color}</h2>
+                        <h1>{pokemon.name}</h1>
+                        <h2>{pokemon.color}</h2>
                     </div>
               )})}
+        </div>
         </div>
     );
 }
