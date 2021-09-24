2cd5201675d817c667feedb5ca82763c98d17e2d
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-09-24 11:37:47 -0300

    fixed functionality, pokemonlist return not rendering

--- src/Components/PokemonListItemComponent.jsx
+++ src/Components/PokemonListItemComponent.jsx
@@ -1,23 +1,22 @@
-
-
-            import React from 'react'; 
+import React from 'react'; 
 import "../styles/output.css";
 
 
 const PokemonListItem = ({pokemon}) => {
-   return (
+   console.log(pokemon.id);
+    return (
+       
     <div>
         <span>{pokemon.name}</span>
-        <div className={`box-border h-32 w-32 m-2 border-4 rounded-xl border-${pokemon.color} block`}>
-        <div className="flex justify-end p-1">
-            <span>{pokemon.id}</span>
-        </div>
-    {/*<img src={`./src/Images/${pokemon.img}`} alt={pokemon.name}/>*/}
-        <div className={`bg-${pokemon.color} w-32 rounded-xl flex justify-center -mx-1 my-9 p-1`}>
-            <span>{pokemon.name}</span>
-        </div>
-        
-    </div>
+            <div className={`box-border h-32 w-32 m-2 border-4 rounded-xl border-${pokemon.color} block`}>
+                <div className="flex justify-end p-1">
+                    <span>{pokemon.id}</span>
+                </div>
+                {/*<img src={`./src/Images/${pokemon.img}`} alt={pokemon.name}/>*/}
+                <div className={`bg-${pokemon.color} w-32 rounded-xl flex justify-center -mx-1 my-9 p-1`}>
+                    <span>{pokemon.name}</span>
+                </div>
+            </div>
         {/*<img src={`../Images/${pokemon.img}`} alt={pokemon.name}/>*/}
    </div>
    
--- src/Components/Pokemonlist.jsx
+++ src/Components/Pokemonlist.jsx
@@ -8,16 +8,12 @@ export default function Pokemonlist ({ pokemonsJSON }) {
 
     const getData = async url => {
         let response = await getPokemonsData(url);
-        console.log(response.species.url);
         const color = await getPokemonColor(response.species.url);
-        console.log(color);
         response.color = color;
         setPokemons([...pokemons, response]);
     };
 
     useEffect(() => {
-        console.log(pokemonsJSON);
-        console.log(pokemonsJSON[1].url);
         pokemonsJSON && pokemonsJSON.map((pokemon) => {
             getData(pokemon.url);
         });
@@ -25,16 +21,7 @@ export default function Pokemonlist ({ pokemonsJSON }) {
     },[pokemonsJSON]);
 
     return (
-        <div>
-        <h2>Pokemon Name</h2>
-        <div>
-        {pokemons && pokemons.map((pokemon) => {
-                   return (
-                    <div>
-                        <PokemonListItem key={pokemon.id} pokemon={pokemon}></PokemonListItem>
-                    </div>
-              )})}
-        </div>
-        </div>
-    );
+        pokemons && pokemons.map((pokemon) => {
+            <h1>{pokemon.id}</h1>        
+        }));
 }
