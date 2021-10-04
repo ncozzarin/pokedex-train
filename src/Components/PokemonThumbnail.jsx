da8dccb19163ed6028dc3126add870cd831ad42b
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-10-04 16:09:31 -0300

    pokemon thumbnail component created

---
+++ src/Components/PokemonThumbnail.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'; 
+import "../styles/output.css";
+
+
+const PokemonThumbnail = ({imgUrl}) => {
+    const [PokemonThumbnail, setPokemonThumbnail] = useState();
+
+    return (
+        
+   );
+}
+ 
+export default PokemonListItem;
