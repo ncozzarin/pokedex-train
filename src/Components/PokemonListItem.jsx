a33bb80726fa17f843172170f788287941b9cfba
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-10-06 16:25:37 -0300

    Update style with flex

--- src/Components/PokemonListItem.jsx
+++ src/Components/PokemonListItem.jsx
@@ -5,15 +5,15 @@ import PokemonThumbnail from '../Components/PokemonThumbnail';
 
 const PokemonListItem = ({pokemon}) => {
     return (
-            <div className={`w-24 h-28 bg-white box-border border-2 rounded-xl border-${pokemon.color}-400 mb-2`}>
-                <div>
-                    <div className={`flex justify-end mx-1 text-${pokemon.color}-400 `}>
+            <div className="flex  justify-center align-center">
+                <div className={`w-24 h-28 bg-white box-border border-2 rounded-xl border-${pokemon.color}-400 mb-2`}>
+                    <div className={`flex justify-end mx-1 text-${pokemon.color}-400  text-xs`}>
                         <h3>{`#0${pokemon.id}`}</h3>
                     </div>
                     <div className="flex justify-center align-center -my-4">
                         <PokemonThumbnail key={pokemon.id} imgUrl={pokemon.sprites.front_default}></PokemonThumbnail>
                     </div>
-                    <div className={`bg-${pokemon.color}-400 w-23 pb-1 rounded-xl flex justify-center text-white `}>
+                    <div className={`w-23 bg-${pokemon.color}-400 rounded-l flex justify-center text-white my-2 `}>
                         <h3>{pokemon.name}</h3>
                     </div>
                 </div>
