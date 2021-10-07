c9ee6a7b442a5cc657a58250f2a244e4d64eaa43
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-10-07 11:30:41 -0300

    style improved

--- src/Components/PokemonListItem.jsx
+++ src/Components/PokemonListItem.jsx
@@ -6,16 +6,18 @@ import PokemonThumbnail from '../Components/PokemonThumbnail';
 const PokemonListItem = ({pokemon}) => {
     return (
             <div className="flex  justify-center align-center">
-                <div className={`w-24 h-28 bg-white box-border border-2 rounded-xl border-${pokemon.color}-400 mb-2`}>
-                    <div className={`flex justify-end mx-1 text-${pokemon.color}-400  text-xs`}>
+                <div className={`w-auto h-28 bg-white box-border border-2 rounded-xl border-${pokemon.color}-400 mb-2`}>
+                    <div className={`flex justify-end mx-1 text-${pokemon.color}-400   text-xs`}>
                         <h3>{`#0${pokemon.id}`}</h3>
                     </div>
                     <div className="flex justify-center align-center -my-3">
                         <PokemonThumbnail key={pokemon.id} imgUrl={pokemon.sprites.front_default}></PokemonThumbnail>
                     </div>
-                    <div className={`w-23 bg-${pokemon.color}-400 rounded-l flex justify-center text-white `}>
+                    <div className={`w-auto bg-${pokemon.color}-400 -my-5 flex justify-center text-white `}>
                         <h3>{pokemon.name}</h3>
                     </div>
+                    <div className={`w-full bg-${pokemon.color}-400 h-2 my-4 bg-red-400 rounded-xl`}></div>
+
                 </div>
             </div>
    );
