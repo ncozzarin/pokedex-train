9d5c10e6a577c5212a420a42b0ae1aaea2ac6cba
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-10-09 12:14:25 -0300

    Minimum hover effect

--- src/Components/PokemonListItem.jsx
+++ src/Components/PokemonListItem.jsx
@@ -6,7 +6,7 @@ import PokemonThumbnail from '../Components/PokemonThumbnail';
 const PokemonListItem = ({pokemon}) => {
     return (
             <div className="flex  justify-center align-center">
-                <div className={`w-auto h-28 bg-white box-border border-2 rounded-xl border-${pokemon.color}-400 mb-2`}>
+                <div className={`group w-auto h-28 bg-white box-border border-2 rounded-xl border-${pokemon.color}-400 hover:shadow-lg cursor-pointer mb-2`}>
                     <div className={`flex justify-end mx-1 text-${pokemon.color}-400   text-xs`}>
                         <h3>{`#0${pokemon.id}`}</h3>
                     </div>
@@ -16,7 +16,7 @@ const PokemonListItem = ({pokemon}) => {
                     <div className={`w-auto bg-${pokemon.color}-400 -my-5 flex justify-center text-white `}>
                         <h3>{pokemon.name}</h3>
                     </div>
-                    <div className={`w-full bg-${pokemon.color}-400 h-3 my-4 sm:h-2 sm:my-4 bg-red-400 rounded-xl`}></div>
+                    <div className={`w-full bg-${pokemon.color}-400 h-3 my-4 sm:h-2 sm:my-4 rounded-xl`}></div>
 
                 </div>
             </div>
