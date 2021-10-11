081cabf3c35fe24203874f646a95d311a424b4f0
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-10-11 20:52:38 -0300

    importing router component in the listItem component

--- src/Components/PokemonListItem.jsx
+++ src/Components/PokemonListItem.jsx
@@ -1,7 +1,8 @@
 import React from 'react'; 
 import "../styles/output.css";
 import PokemonThumbnail from '../Components/PokemonThumbnail';
-
+import Detailscreen from '../Layouts/Detailscreen';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const PokemonListItem = ({pokemon}) => {
     return (
