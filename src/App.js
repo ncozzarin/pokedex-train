2422f9af5ed68ac033b652ac34d0ded2edf44ed8
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-10-11 21:19:26 -0300

    update imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./styles/output.css";
 import "./Components/Pokemonlist";
-import HomeScreen from './Layouts/Homescreen';
-import Detailscreen from '../Layouts/Detailscreen';
+import HomeScreen from '../src/Layouts/Homescreen';
+import Detailscreen from '../src/Layouts/Detailscreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
