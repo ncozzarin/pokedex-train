ca9cf01d4b3f76425c09b4c42077542ec35cad05
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-10-24 15:42:38 -0300

    editing styles

--- src/Components/PokemonListItem.jsx
+++ src/Components/PokemonListItem.jsx
@@ -13,11 +13,9 @@ const PokemonListItem = ({pokemon}) => {
                     <div className="flex justify-center align-center -my-3">
                         <PokemonThumbnail key={pokemon.id} imgUrl={pokemon.sprites.front_default}></PokemonThumbnail>
                     </div>
-                    <div className={`w-auto bg-${pokemon.color}-400 -my-5 flex justify-center text-white `}>
+                    <div className={`w-auto thumbnail-base-shape bg-${pokemon.color}-400 -my-5 flex justify-center text-white `}>
                         <h3>{pokemon.name}</h3>
                     </div>
-                    <div className={`w-full bg-${pokemon.color}-400 h-3 my-4 sm:h-2 sm:my-4 rounded-xl`}></div>
-
                 </div>
             </div>
    );
