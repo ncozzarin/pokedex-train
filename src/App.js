cf51d82a59c9acfc63b146ec91efe8754b5b729a
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-11-05 14:41:19 -0300

    creating pokemon action for redux

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import "./styles/output.css";
 import "./Components/Pokemonlist";
 import HomeScreen from '../src/Layouts/Homescreen';
 import Detailscreen from '../src/Layouts/Detailscreen';
+import { fetchPokemons } from './redux/actions/pokemons';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(props) {
-  const data = useSelector(state => {
-    console.log(state);
-  });
   return (
     <Router>
       <Route path="/" component={HomeScreen}/>
