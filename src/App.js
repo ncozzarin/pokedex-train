8b0d65439342e7c71871371a20089375b4d07b58
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-11-06 17:12:59 -0300

    fetch pokemons action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import "./styles/output.css";
 import "./Components/Pokemonlist";
 import HomeScreen from '../src/Layouts/Homescreen';
@@ -8,6 +8,16 @@ import { fetchPokemons } from './redux/actions/pokemons';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(props) {
+  const dispatch = useDispatch();
+  const [pokemons, setPokemons] = useState([]);
+  
+  useEffect(() => {
+    if (pokemons.length === 0){
+      dispatch(fetchPokemons());
+      setPokemons([{},{}]);
+    }
+  })
+  
   return (
     <Router>
       <Route path="/" component={HomeScreen}/>
