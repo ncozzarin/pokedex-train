67e97dd2eedf6d69dcda345e8931e73f43a6df75
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-11-09 20:58:29 -0300

    Definition , creation and configuration of the redux store

---
+++ src/features/pokemons/pokemonsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  {}
+]
+
+const pokemonsSlice = createSlice({
+  name: 'pokemons',
+  initialState,
+  reducers: {}
+})
+
+export default pokemonsSlice.reducer
--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import storeFn from './redux/store';
-
-
-const store = storeFn();
-
-
+import store from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
