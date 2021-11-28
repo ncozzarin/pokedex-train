e4fa4b93e42a7d73d90e4aebfcc228030538a4c2
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-11-28 00:35:49 -0300

    Fixed pokemons slice

--- src/features/pokemons/pokemonsSlice.js
+++ src/features/pokemons/pokemonsSlice.js
@@ -1,66 +1,67 @@
-import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-  pokemons:[],
-  status: 'idle',
-  error: null
-}
+    pokemons: [],
+    status: 'idle',
+    error: null,
+};
 
-const pokemonsSlice = createSlice({
-  name: 'pokemons',
-  initialState,
-  reducers: {
-    addPokemons: state => {
-        state.pokemons.push(action.payload)
-      }
-    }
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(fetchPokemons.pending, (state, action) => {
-        state.status = 'loading'
-      })
-      .addCase(fetchPokemons.fulfilled, (state, action) => {
-        state.status = 'succeeded'
-        // Add any fetched pokemons to the array
-        state.pokemons = state.pokemons.concat(action.payload)
-      })
-      .addCase(fetchPokemons.rejected, (state, action) => {
-        state.status = 'failed'
-        state.error = action.error.message
-      })
-  }
-})
+export const fetchPokemons = createAsyncThunk(
+    'pokemons/fetchPokemons',
+    async (limitNumber, offsetNumber) => {
+        //Aca hardcodee el offsetNumber
+        const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon?limit=${limitNumber}&offset=3`,
+        );
+        const data = await response.json();
+        return data;
+    },
+);
 
-export const fetchPokemons = createAsyncThunk('pokemons/fetchPokemons', async () => async (limitNumber, offsetNumber) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limitNumber}&offset=${offsetNumber}`);
-  // const data = await response.json();
-  console.log(response.data)
-  useDispatch(addPokemons());
-  return response.data;
-})
-
-export const fetchPokemonsData = createAsyncThunk('pokemons/getPokemonsData', async (pokemonUrl) => {
-  const response = await fetch(`${pokemonUrl}`);
-  const data = await response.json();
-  return data;
-})
+export const fetchPokemonsData = createAsyncThunk(
+    'pokemons/getPokemonsData',
+    async (pokemonUrl = '') => {
+        console.log({ pokemonUrl });
+        const response = await fetch(`${pokemonUrl}`);
+        return response.json();
+    },
+);
 
 export const fetchPokemonsColor = async (pokemonSpeciesUrl) => {
-  const response = await fetch(`${pokemonSpeciesUrl}`);
-  const data = await response.json();
-  return data.color.name;
-}
+    const response = await fetch(`${pokemonSpeciesUrl}`);
+    const data = await response.json();
+    return data.color.name;
+};
 
-export const selectAllPokemnons = state => state.pokemons
+export const selectAllPokemnons = (state) => state.pokemons;
 
 export const selectPokemonById = (state, pokemonId) =>
-  state.pokemons.find(pokemon => pokemon.id === pokemonId)
+    state.pokemons.find((pokemon) => pokemon.id === pokemonId);
 
+const pokemonsSlice = createSlice({
+    name: 'pokemons',
+    initialState,
+    reducers: {
+        addPokemons: (state, action) => {
+            state.pokemons.push(action.payload);
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPokemons.pending, (state, action) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchPokemons.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                // Add any fetched pokemons to the array
+                state.pokemons = state.pokemons.concat(action.payload);
+            })
+            .addCase(fetchPokemons.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            });
+    },
+});
 
-  
-export default pokemonsSlice.reducer
-
-// codigo de mati
-
+export default pokemonsSlice.reducer;
