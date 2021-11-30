901c2fdac6a41694379dc7e0fe8299e07e2d5a02
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-11-30 11:24:10 -0300

    Update pokemonList to fetch the pokemon data
    The approach i am going to take is to get in the store state the name and the url of each pokemon, and the details just save them in the local storage of each component

--- src/Components/Pokemonlist.jsx
+++ src/Components/Pokemonlist.jsx
@@ -5,30 +5,14 @@ import { selectAllPokemons } from '../features/pokemons/pokemonsSlice'
 import { useDispatch, useSelector } from 'react-redux';
 //Redux logic to consume pokemons frm store with useSelector
 export const Pokemonlist = () => {
+    const [pokemons, setPokemons] = useState([]);
+    let pokemonsArray = [];
     const pokemons = useSelector(state => state.pokemons);
     console.log(pokemons)
     const dispatch = useDispatch();
-    const getData = async (url, i) => {
-        let response = await useDispatch(url);
-        const color = await getPokemonColor(response.species.url);
-        response.color = color;
-        pokemonsArray.push(response);
-        if (i === pokemonsJSON.length - 1)
-            setPokemons(pokemonsArray);
-        return response;
-    };
-    const renderedPokemons = pokemons.pokemons && pokemons.pokemons.map(pokemon => (
-        <PokemonListItem key={pokemon.id} pokemon={pokemon}></PokemonListItem>
-    ))
-    return(<h1>hola</h1>)
-}
-
-/* const Pokemonlist = ({ pokemonsJSON }) => {
-    const [pokemons, setPokemons] = useState([]);
-    let pokemonsArray = [];
 
     const getData = async (url, i) => {
-        let response = await getPokemonsData(url);
+        let response = await dispatch(url);
         const color = await getPokemonColor(response.species.url);
         response.color = color;
         pokemonsArray.push(response);
@@ -40,22 +24,13 @@ export const Pokemonlist = () => {
     useEffect(() => {
         pokemonsJSON && pokemonsJSON.map((pokemon, i) => {
             getData(pokemon.url, i);
-            return pokemon.url;
         });
-        return pokemonsJSON;
     }, [pokemonsJSON]);
 
-    console.log(pokemons);
-    return (
-        
-        <div className="grid gap-1 grid-cols-3 mx-3">
-            {
-
-            }
-        </div>
-    )
-
-
-} */
+    const renderedPokemons = pokemons.pokemons && pokemons.pokemons.map(pokemon => (
+        <PokemonListItem key={pokemon.id} pokemon={pokemon}></PokemonListItem>
+    ))
+    return(<h1>hola</h1>)
+}
 
 export default Pokemonlist;
