c66b57140ed41218791d3e376df3fdf54babd1b6
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-12-09 23:49:06 -0300

    fix capitalice function

--- src/Components/PokemonTypes.jsx
+++ src/Components/PokemonTypes.jsx
@@ -1,4 +1,3 @@
-import { iteratorSymbol } from 'immer/dist/internal';
 import React from 'react';
 import "../styles/output.css";
 
@@ -6,15 +5,16 @@ import "../styles/output.css";
 
 const PokemonTypes = ({types, color}) => {
 
-    function capitalize(str) {
-        const lower = str.toLowerCase();
-        return str.charAt(0).toUpperCase() + lower.slice(1);
-      }
-    
-      types && types.map(type => type.type.name = capitalize(type.type.name) 
+  function capitalize(str) {
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+    } 
 
-    return(
-    types && types.map(type => <h3 className={`bg-${color}-400 rounded-xl p-1 m-2 h-auto text-white text-l font-bold`}>{type.type.name} </h3>)
+    
+  types && types.map(type => {
+    type = capitalize(type);
+    return(<h3 className={`bg-${color}-400 rounded-xl p-1 m-2 h-auto text-white text-l font-bold`}>{type.type.name} </h3>);
+  }
     );
 }
  
